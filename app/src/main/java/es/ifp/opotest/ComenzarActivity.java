7f06c066657bd5803b8d9ad5c45b347e50e8d137
Adrian <adriandelavl1@gmail.com>
2024-04-14 21:00:03 +0200

    he añadido un comentario a ComenzarActivity

--- app/src/main/java/es/ifp/opotest/ComenzarActivity.java
+++ app/src/main/java/es/ifp/opotest/ComenzarActivity.java
@@ -10,5 +10,7 @@ public class ComenzarActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comenzar);
+
+        //// hollalalalalalalalalalalalalalalalalal
     }
 }
