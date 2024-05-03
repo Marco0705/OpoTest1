0bc0837a73a76892b28a066cb0516005d47baef9
Marco0705 <marco070593@gmail.com>
2024-05-03 18:00:40 +0200

    falta revisar entregable 7 y 8, hacer bonita la interfaz

--- app/src/main/java/es/ifp/opotest/Funciones.java
+++ app/src/main/java/es/ifp/opotest/Funciones.java
@@ -6,7 +6,6 @@ import android.database.sqlite.SQLiteDatabase;
 
 public class Funciones {
 
-    protected DataBaseSQL db;
 
     public boolean contrasena(String a, String b) {
         if (a.equals(b)) {
@@ -16,11 +15,9 @@ public class Funciones {
 
 
     }
-    //otra funcion
     public boolean campos(String nombre, String email, String pass1, String pass2, String cuenta, boolean radio1, boolean radio2, boolean check1){
-        if (nombre.equals("") || email.equals("") || pass1.equals("") || pass2.equals("") || cuenta.equals("") || ((!radio1) && (!radio2)) || (!check1)) {
+        if (nombre.equals("") || email.equals("") || pass1.equals("") || pass2.equals("") || cuenta.equals("") || ((!radio1 && !radio2)) || !check1) {
             return false;
-
         }
         return true;
 
@@ -33,5 +30,18 @@ public class Funciones {
         return false;
 
     }
+    public String resultado(int a, int b){
+        if(a > b){
+
+            return "Aprobado";
+        }
+        else{
+            return "Suspendido";
+        }
+
+    }
+
+
+
 
 }
